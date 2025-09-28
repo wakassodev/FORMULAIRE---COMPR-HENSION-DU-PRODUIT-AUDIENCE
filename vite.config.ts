16054c6347d6e94915e16666d6ca1b00fe353fa7
wakassodev <wakasso94@gmail.com>
2025-09-28 16:40:25 +0400

    adding the vercel file

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
-
-  import { defineConfig } from 'vite';
+import { defineConfig } from 'vite';
   import react from '@vitejs/plugin-react-swc';
   import path from 'path';
 
@@ -51,7 +50,7 @@
     },
     build: {
       target: 'esnext',
-      outDir: 'build',
+      outDir: 'dist',
     },
     server: {
       port: 3000,
